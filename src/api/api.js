29c443a825ff00e1f3438ee40b9d51fdcad54abf
krisboz <kristijan.bozinovic1@gmail.com>
2025-04-02 23:17:19 +0200

    .

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://reimagined-journey-5r599v49g9r2577-5000.app.github.dev"
+const API_BASE_URL =
+  "https://reimagined-journey-5r599v49g9r2577-5000.app.github.dev";
 
 //const API_BASE_URL = "http://localhost:5000"; // Change this to your actual backend URL
 
